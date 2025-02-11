4bc779c98e1dd710037b3633a871c73681385308
Satish Patil <syapatil0803@gmail.com>
2025-02-11 14:24:24 +0530

    .

--- src/main/java/com/microservices/users/configuration/SecurityConfig.java
+++ src/main/java/com/microservices/users/configuration/SecurityConfig.java
@@ -21,7 +21,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers(
                                         "/users/login",
